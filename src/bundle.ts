80bc11853db0afdf88f5feb9176f93ae3e28e18a
Dr. Carsten Leue <carsten.leue@gmx.net>
2020-09-12 21:35:38 +0200

    add verbose option

--- src/bundle.ts
+++ src/bundle.ts
@@ -1,29 +1,49 @@
-import { green, grey, red } from "chalk";
-import { Command } from "commander";
-import { join } from "path";
-import { argv } from "process";
-import { concat, merge, of } from "rxjs";
-import { ignoreElements, map, takeUntil } from "rxjs/operators";
+import { green, grey, red } from 'chalk';
+import { Command } from 'commander';
+import { join } from 'path';
+import { argv } from 'process';
+import { concat, merge, of, pipe } from 'rxjs';
+import { ignoreElements, map, takeUntil } from 'rxjs/operators';
 
-import { version } from "../package.json";
-import { sync } from "./sync";
+import { version } from '../package.json';
+import { sync } from './sync';
+
+// verbose
+const stdOutVerbose = map<string[], void>((line) => console.log(grey(...line)));
+const stdErrVerbose = map<string, void>((line) => console.error(red(line)));
+const stdVerbose = pipe(
+  map<string[], void>((line) => console.log(green(join(...line)))),
+  ignoreElements()
+);
+
+// quiet
+const stdOutQuiet = ignoreElements();
+const stdErrQuiet = ignoreElements();
+const stdQuiet = ignoreElements();
 
 // exectue
 const program = new Command();
 program
   .version(version)
   .name("cbt")
+  .description("simple backup utility")
   .arguments("<source> <target>")
+  .option("-v, --verbose", "verbose output", false)
+  .option("-q, --quiet", "quiet output", false)
   .action((src: string, dst: string) => {
+    // check verbose output
+    const bVerbose = !!program.verbose;
+    const bQuiet = !!program.quiet;
     // sync result
     const { file$, stdout$, stderr$ } = sync(src, dst);
+    // filters
+    const outFilter = bVerbose ? stdOutVerbose : stdOutQuiet;
+    const errFilter = bVerbose ? stdErrVerbose : stdErrQuiet;
+    const stdFilter = bQuiet ? stdQuiet : stdVerbose;
     // attach
-    const out$ = stdout$.pipe(map((line) => console.log(grey(...line))));
-    const err$ = stderr$.pipe(map((line) => console.error(red(...line))));
-    const line$ = file$.pipe(
-      map((line) => console.log(green(join(...line)))),
-      ignoreElements()
-    );
+    const out$ = stdout$.pipe(outFilter);
+    const err$ = stderr$.pipe(errFilter);
+    const line$ = file$.pipe(stdFilter);
     // start debugging
     const done$ = concat(line$, of(true));
     // merge everything
